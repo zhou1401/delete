16ba6ad8a17891dc2809245e18eb9f567edaf0a4
kiage <1006654899@qq.com>
2017-11-12 16:57:31 +0800

    Basic code

---
+++ delete/delete/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  delete
+//
+//  Created by fzgh on 2017/11/12.
+//  Copyright © 2017年 kaige. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ delete/delete/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  delete
+//
+//  Created by fzgh on 2017/11/12.
+//  Copyright © 2017年 kaige. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
